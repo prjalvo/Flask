545d84150d31ab6f2fb4af965e5dcca4d9ab8af3
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 19:58:14 -0300

    Update settings.py

--- flaskblog/settings.py
+++ flaskblog/settings.py
@@ -77,7 +77,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
