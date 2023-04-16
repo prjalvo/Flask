6a263f730f2c1c2815b0ba0fbd99ebf54a8d6cb8
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 16:36:40 -0300

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -13,3 +13,5 @@ class Config:
     
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
