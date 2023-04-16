1614661d66929a28527e02a7fc67295daaa536d7
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 17:02:42 -0300

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -10,6 +10,15 @@ class Config:
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
+
+    
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get('EMAIL_USER')
+MAIL_PASSWORD = os.environ.get('EMAIL_PASS')    
     
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
