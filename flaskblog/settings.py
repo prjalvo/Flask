03d16ce513342e6773ebdc799c508caa3663df11
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 20:49:04 -0300

    Update settings.py

--- flaskblog/settings.py
+++ flaskblog/settings.py
@@ -12,10 +12,7 @@ import os
 
 from django.conf import settings
 
-settings.configure(
-    ROOT_URLCONF=__name__,
-)
-
+ROOT_URLCONF=__name__
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
