ad8f22d5f73b3e0fe4b140d19053823acd55f88a
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 21:38:48 -0300

    Update urls.py

--- flaskblog/urls.py
+++ flaskblog/urls.py
@@ -1,27 +1,9 @@
-"""backend URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings # new
 from  django.conf.urls.static import static #new
-from flaskblog import views
-from flaskblog.result_view import SubmissionDetailView
-
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,10 +15,7 @@ schema_view = get_schema_view(
     public=True,
 )
 
-urlpatterns = [    
-    path('reset_password/', views.reset_password, name='reset_password'),
-    path('', views.index, name='index'),
-]
 
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-#urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
