50a073f60bb725b72bacc8f29e1af6c00889eb48
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 22:51:07 -0300

    Update models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -3,6 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
+from rest_framework.views import APIView
 
 
 @login_manager.user_loader
@@ -10,7 +11,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin,APIView):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
