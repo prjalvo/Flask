594479f66c0ac5d66e5dd633a57b070fcccbebba
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 22:00:39 -0300

    Update settings.py

--- flaskblog/settings.py
+++ flaskblog/settings.py
@@ -28,6 +28,17 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
