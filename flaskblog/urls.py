7ea758c750e11a1b9e5848ecce006ecd7f5e4027
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 23:04:37 -0300

    Update urls.py

--- flaskblog/urls.py
+++ flaskblog/urls.py
@@ -3,6 +3,6 @@ from flaskblog.models import User
 
 urlpatterns = [
     
-    path('reset_passwd/', User.as_view(), name='get_reset_token'),
+    path(r'reset_passwd', User.as_view(), name='get_reset_token'),
 ]
 
