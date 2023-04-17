b10c5cc746fcde7d55e41c5de0d6851d3f396895
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 22:32:17 -0300

    Update urls.py

--- flaskblog/urls.py
+++ flaskblog/urls.py
@@ -1,24 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.conf import settings # new
-from  django.conf.urls.static import static #new
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Backend Online Shop APIS",
-        default_version='1.0.0',
-        description="This is swagger for our apis."
-
-    ),
-    public=True,
-)
+from django.urls import path
+from flaskblog.models import OrderView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
+    path('order/', OrderView.as_view(), name='order'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
