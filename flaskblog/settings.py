60545dafc9a6fccc7b0ba3d51fd0131081b98609
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 21:26:27 -0300

    Update settings.py

--- flaskblog/settings.py
+++ flaskblog/settings.py
@@ -13,26 +13,6 @@ import os
 from django.conf import settings
 
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-  ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 
 ROOT_URLCONF = 'flaskblog.urls'
 
