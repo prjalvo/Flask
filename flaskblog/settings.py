1f4cc9279b2f24576b98b59de16d7e9c5fa2551e
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2023-04-16 22:04:39 -0300

    Update settings.py

--- flaskblog/settings.py
+++ flaskblog/settings.py
@@ -49,6 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+WSGI_APPLICATION = 'flaskblog.wsgi.application'
 
 ALLOWED_HOSTS = ['*']
 # Application definition
